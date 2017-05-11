563eb8d50eef2eb47bd57d35d010320431de5eee
Damian Mooyman <damian@silverstripe.com>
2017-05-12 11:46:41 +1200

    BUG fix issue with windows paths Fixes #14

---
+++ tests/php/PublicAssetAdapterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SilverStripe\Assets\Tests;
+
+use SilverStripe\Assets\Flysystem\PublicAssetAdapter;
+use SilverStripe\Control\Director;
+use SilverStripe\Dev\SapphireTest;
+
+class PublicAssetAdapterTest extends SapphireTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        Director::config()->set(
+            'alternate_base_url',
+            'http://www.mysitem.com/baseurl/'
+        );
+    }
+
+    public function testInitBaseURL()
+    {
+        // Test windows paths generate correct url
+        $base = str_replace('/', '\\', BASE_PATH) . '\\assets\\subdir';
+        $adapter = new PublicAssetAdapter($base);
+
+        $this->assertEquals(
+            '/baseurl/assets/subdir/dir/file.jpg',
+            $adapter->getPublicUrl('dir/file.jpg')
+        );
+    }
+}

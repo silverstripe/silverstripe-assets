5dbc09047ce030828a2094839a0dc056c19f33a1
Maxime Rainville <maxime@silverstripe.com>
2021-03-08 14:59:32 +1300

    Rewrite FilesystemTest.php to use mikey179/vfsstream

--- tests/php/FilesystemTest.php
+++ tests/php/FilesystemTest.php
@@ -2,73 +2,125 @@
 
 namespace SilverStripe\Assets\Tests;
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use SilverStripe\Assets\Filesystem;
 use SilverStripe\Dev\SapphireTest;
 
 class FilesystemTest extends SapphireTest
 {
-    public function testRemoveFolderNormalCase()
+
+    /**
+     * Config directory name.
+     *
+     * @var string
+     */
+    protected $directory = 'config';
+
+    /**
+     * Root directory for virtual filesystem
+     *
+     * @var vfsStreamDirectory
+     */
+    protected $root;
+
+    protected function setUp()
     {
-        // Create a temporary folder
-        $path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'silverstripe-assets';
-        Filesystem::removeFolder($path);
-        mkdir($path);
-        // Chuck a "normal" folder inside it
-        $normalFolderPath = $path . DIRECTORY_SEPARATOR . 'normal';
-        mkdir($normalFolderPath);
-        // Ensure the folder inside exists
-        $this->assertTrue(is_dir($normalFolderPath));
-        // Remove the folder and ensure it's no longer there
-        Filesystem::removeFolder($normalFolderPath);
-        $this->assertFalse(is_dir($normalFolderPath));
-        // Add that folder back in
-        mkdir($normalFolderPath);
-        // And put a file inside
-        $filePath = $normalFolderPath . DIRECTORY_SEPARATOR . 'example-file';
-        file_put_contents($filePath, 'example');
-        // Ensure our path is still there and the file exists
-        $this->assertTrue(is_dir($path));
-        $this->assertTrue(is_file($filePath));
-        // Remove the path
-        Filesystem::removeFolder($path);
-        // Ensure it was removed
-        $this->assertFalse(is_dir($path));
-        // Ensure the "normal" folder was removed as part of the recursive removal
-        $this->assertFalse(is_dir($normalFolderPath));
-        // Ensure our file was deleted too
-        $this->assertFalse(is_file($filePath));
+        parent::setUp();
+        $this->root = vfsStream::setup(
+            'root',
+            null,
+            [
+                'rootfile.txt' => 'hello world',
+                'empty-folder' => [],
+                'not-empty-folder' => [
+                  'file.txt' => 'I\'m a file'
+                ],
+                'folder-with-subfolders' => [
+                    'empty-subfolder' => [],
+                    'not-empty-subfolder' => [
+                        'file-in-sub-folder.txt' => 'I am in a subfolder'
+                    ],
+                ],
+                'folder-with-falsy-file' => [
+                    '0' => 'If you parse my filename as an int, I will be false'
+                ]
+            ]
+        );
     }
 
-    public function testRemoveFolderFalsieNames()
+    private function buildPath(): string
     {
-        // Create a temporary folder
-        $path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'silverstripe-assets';
-        mkdir($path);
-        // Create a file with the name `0` which evaluates to false
-        $filePath = $path . DIRECTORY_SEPARATOR . '0';
-        file_put_contents($filePath, 'example');
-        // Try to remove the folder and file recursively
-        Filesystem::removeFolder($path);
-        // Ensure they were both removed
-        $this->assertFalse(is_dir($path));
-        $this->assertFalse(is_file($filePath));
+        $nodes = func_get_args();
+        array_unshift($nodes, $this->root->url());
+        return implode(DIRECTORY_SEPARATOR, $nodes);
+    }
+
+    public function testRemoveEmptyFolder()
+    {
+        $folderPath = $this->buildPath('empty-folder');
+        $this->assertDirectoryExists($folderPath);
+
+        Filesystem::removeFolder($folderPath);
+        $this->assertDirectoryNotExists($folderPath);
+    }
+
+    public function testRemoveFolderWithFiles()
+    {
+        $folderPath = $this->buildPath('not-empty-folder');
+        $filePath = $this->buildPath('not-empty-folder', 'file.txt');
+        $this->assertDirectoryExists($folderPath);
+        $this->assertFileExists($filePath);
+
+        Filesystem::removeFolder($folderPath);
+        $this->assertDirectoryNotExists($folderPath);
+        $this->assertFileNotExists($filePath);
+    }
+
+    public function testRemoveFolderWithSubFolder()
+    {
+        $folderPath = $this->buildPath('folder-with-subfolders');
+        $emptySubfolder = $this->buildPath('folder-with-subfolders', 'empty-subfolder');
+        $filePath = $this->buildPath('folder-with-subfolders', 'not-empty-subfolder', 'file-in-sub-folder.txt');
+
+        $this->assertDirectoryExists($folderPath);
+        $this->assertDirectoryExists($emptySubfolder);
+        $this->assertFileExists($filePath);
+
+        Filesystem::removeFolder($folderPath);
+        $this->assertDirectoryNotExists($folderPath);
+        $this->assertDirectoryNotExists($emptySubfolder);
+        $this->assertFileNotExists($filePath);
+    }
+
+    public function testRemoveFolderWithFalsyFile()
+    {
+        $folderPath = $this->buildPath('folder-with-falsy-file');
+        $filePath = $this->buildPath('folder-with-falsy-file', '0');
+        $this->assertDirectoryExists($folderPath);
+        $this->assertFileExists($filePath);
+
+        Filesystem::removeFolder($folderPath);
+        $this->assertDirectoryNotExists($folderPath);
+        $this->assertFileNotExists($filePath);
     }
 
     public function testRemovedFolderContentsOnly()
     {
-        // Create a temporary folder
-        $path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'silverstripe-assets';
-        mkdir($path);
-        // Create a file with the name `0` which evaluates to false
-        $filePath = $path . DIRECTORY_SEPARATOR . '0';
-        file_put_contents($filePath, 'example');
-        // Try to remove the folder and file recursively
-        Filesystem::removeFolder($path, true);
-        // Ensure the folder still exists
-        $this->assertTrue(is_dir($path));
-        // Ensure the asset doesn't exist
-        $this->assertFalse(is_file($filePath));
-        // Finally clean up after ourselves
-        Filesystem::removeFolder($path);
+        $folderPath = $this->buildPath('folder-with-subfolders');
+        $emptySubfolder = $this->buildPath('folder-with-subfolders', 'empty-subfolder');
+        $notEmptySubfolder = $this->buildPath('folder-with-subfolders', 'not-empty-subfolder');
+        $filePath = $this->buildPath('folder-with-subfolders', 'not-empty-subfolder', 'file-in-sub-folder.txt');
+
+        $this->assertDirectoryExists($folderPath);
+        $this->assertDirectoryExists($emptySubfolder);
+        $this->assertDirectoryExists($notEmptySubfolder);
+        $this->assertFileExists($filePath);
+
+        Filesystem::removeFolder($folderPath, true);
+        $this->assertDirectoryExists($folderPath);
+        $this->assertDirectoryNotExists($emptySubfolder);
+        $this->assertDirectoryNotExists($notEmptySubfolder);
+        $this->assertFileNotExists($filePath);
     }
 }

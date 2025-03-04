b42ba9d2f1c90faedb2f6d9f37a5a560ae319b19
Steve Boyd <emteknetnz@gmail.com>
2025-03-05 11:15:13 +1300

    ENH Add campaign-admin support back in

--- src/Shortcodes/FileLink.php
+++ src/Shortcodes/FileLink.php
@@ -27,4 +27,12 @@ class FileLink extends DataObject
         'Parent' => DataObject::class,
         'Linked' => File::class,
     ];
+
+    /**
+     * Don't show this model in campaign admin as part of implicit change sets
+     *
+     * @config
+     * @var bool
+     */
+    private static $hide_in_campaigns = true;
 }
